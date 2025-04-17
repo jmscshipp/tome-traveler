1358714c0dad19e5fa2823303fcf54fbf63a93dc
Arthur Hertz <arthertz@iu.edu>
2025-04-16 20:29:20 -0500

    added new map and clear tool

--- Assets/Scripts/ConnectNodeTool.cs
+++ Assets/Scripts/ConnectNodeTool.cs
@@ -10,12 +10,41 @@ public class LocationToolOverlay : Overlay
     {
         var root = new VisualElement() { name = "Location Connector Tool Root" };
         root.Add(new Label() { text = "Location Connector Tool" });
-        Button button = new Button() { text = "Connect selected MapLocations" };
-        button.clicked += OnButtonClick;
-        root.Add(button);
+        Button connectButton = new Button() { text = "Connect selected MapLocations" };
+        Button clearButton = new Button() { text = "Clear selected MapLocations" };
+        connectButton.clicked += ConnectAll;
+        clearButton.clicked += ClearAll;
+        root.Add(connectButton);
+        root.Add(clearButton);
         return root;
     }
-    void OnButtonClick()
+
+    void ClearAll()
+    {
+        if (Selection.GetFiltered<MapLocation>(SelectionMode.TopLevel).Length == 0)
+        {
+            Debug.Log("No map locations selected!");
+            return;
+        }
+
+        IEnumerable<Object> targets = Selection.GetFiltered<MapLocation>(SelectionMode.TopLevel);
+
+        foreach (var obj in targets)
+        {
+            if (!(obj is MapLocation platform))
+                continue;
+
+            foreach (var obj2 in targets)
+            {
+                if (obj == obj2)
+                    continue;
+                Debug.Log("Connecting nodes " + obj.name + " " + obj2.name);
+                ((MapLocation)obj).GetConnectedLocations().Remove(((MapLocation)obj2));
+                ((MapLocation)obj2).GetConnectedLocations().Remove(((MapLocation)obj));
+            }
+        }
+    }
+    void ConnectAll()
     {
         if (Selection.GetFiltered<MapLocation>(SelectionMode.TopLevel).Length == 0)
         {
